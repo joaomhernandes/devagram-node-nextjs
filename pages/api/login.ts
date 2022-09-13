742b3909a90683e8aa46f3e33d915441e3b5a8bc
João Maurício Hernandes Carrenho <78742654+joaomhernandes@users.noreply.github.com>
2022-09-13 19:12:53 -0300

    Versão inicial do Login
    Criação do projeto,  endpoint de Login e lógica de resposta para o login.

---
+++ pages/api/login.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method == "POST") {
+    const { login, senha } = req.body;
+
+    if (login === "admin@admin.com" && senha === "Admin@123") {
+      res.status(200).json({ Msg: "Usuário autenticado com sucesso" });
+    }
+    return res.status(400).json({ Erro: "Usuário ou senha não encontrado" });
+  }
+  return res.status(405).json({ Erro: "Método informado não é válido" });
+};
