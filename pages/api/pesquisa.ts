11cb24711dcdef6532986501bdc2a57463a33fef
João Maurício Hernandes Carrenho <78742654+joaomhernandes@users.noreply.github.com>
2022-09-19 12:17:24 -0300

    Criada API Like/Deslike

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -10,20 +10,34 @@ const pesquisaEndpoint = async (
 ) => {
   try {
     if (req.method === "GET") {
-      const { filtro } = req.query;
-      if (!filtro || filtro.length < 2) {
-        return res.status(400).json({
-          erro: "Favor informar pelo menos 2 caracteres para a busca",
-        });
-      }
+      if (req?.query?.id) {
+        const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id);
+        if (!usuarioEncontrado) {
+          return res.status(400).json({
+            erro: "Usuário não encontrado",
+          });
+        }
+        usuarioEncontrado.senha = null;
+        return res.status(200).json(usuarioEncontrado);
+      } else {
+        const { filtro } = req.query;
+        if (!filtro || filtro.length < 2) {
+          return res.status(400).json({
+            erro: "Favor informar pelo menos 2 caracteres para a busca",
+          });
+        }
 
-      const usuariosEncontrados = await UsuarioModel.find({
-        $or: [
-          { nome: { $regex: filtro, $options: "i" } },
-          { email: { $regex: filtro, $options: "i" } },
-        ],
-      });
-      return res.status(200).json(usuariosEncontrados);
+        const usuariosEncontrados = await UsuarioModel.find({
+          $or: [
+            { nome: { $regex: filtro, $options: "i" } },
+            { email: { $regex: filtro, $options: "i" } },
+          ],
+        });
+        usuariosEncontrados.forEach((userFound) => {
+          userFound.senha = null;
+        });
+        return res.status(200).json(usuariosEncontrados);
+      }
     }
     return res.status(405).json({ erro: "Metodo informado não é valido" });
   } catch (e) {
